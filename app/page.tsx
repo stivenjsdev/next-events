742259bc94ab02a68ac59b26095714f58aee0d0b
stiven trujillo <strujillodev@gmail.com>
2024-10-15 12:48:37 -0500

    feat: server actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <button className="btn btn-primary m-2">Subscribe</button>;
+import Button from "@/components/Button";
+import { getUser } from "@/lib/actions/action";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Home page",
+};
+
+export default async function Home() {
+  const users = await getUser();
+  console.log(users);
+
+  return (
+    <div className="m-2">
+      <Button />
+      <ul>
+        {users.map((user: { id: number; name: string; email: string }) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
